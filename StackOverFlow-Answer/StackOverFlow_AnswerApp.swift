20088098eee089a95cabca2dfa11fc5136fe7473
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-10-20 11:51:33 +0530

    Initial Commit

---
+++ StackOverFlow-Answer/StackOverFlow_AnswerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverFlow_AnswerApp.swift
+//  StackOverFlow-Answer
+//
+//  Created by Heshan Yodagama on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct StackOverFlow_AnswerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
