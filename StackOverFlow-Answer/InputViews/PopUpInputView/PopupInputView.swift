70bd940bfb8d36dc7ccd0c5e2284f5b6491c5bd1
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-11-29 09:57:06 +0530

    update PopupInputView to get selection and selectableItems

--- StackOverFlow-Answer/InputViews/PopUpInputView/PopupInputView.swift
+++ StackOverFlow-Answer/InputViews/PopUpInputView/PopupInputView.swift
@@ -9,32 +9,60 @@ import SwiftUI
 
 
 struct PopupInputView: View {
-    let placeHolder: String
-    var value: String?
+    
+    init(placeHolder: String, selectableItems: [String], selection: Binding<String?>) {
+        self._selection = selection
+        self.placeHolder = placeHolder
+        self.selectableItems = selectableItems
+    }
+    
+    @Binding var selection: String?
+    
+    var placeHolder: String
+    var selectableItems: [String] = []
     
     var body: some View {
-        ZStack {
+        Menu {
+            getMenuItem()
+        } label: {
+            getInputView()
+        }
+    }
+    
+    //MARK: constant and method
+    fileprivate func getMenuItem() -> some View {
+        return ForEach(selectableItems, id: \.self) { (item) in
+            Button {
+                selection = item
+            } label: {
+                HStack {
+                    Text(item)
+                }
+            }
+        }
+    }
+    
+    fileprivate func getInputView() -> some View {
+        return ZStack {
             InputViewBackground()
             HStack {
-                Text(value == nil ? placeHolder:value!)
+                Text(selection == nil ? placeHolder:selection!)
                     .foregroundColor(.yellow)
                 Spacer()
                 RoundBackgroundImage(systemName: "chevron.down", forGroundColor: .gray, length: 19, backgroundColor: .yellow)
             }
-            .padding(/*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .padding(10)
         }
         .frame(height: UIConstants.height, alignment: .center)
-        //TODO: Add tap geture
     }
-    
-    //MARK: constant and method
     
 }
 struct PopupInputView_Previews: PreviewProvider {
+    @State static var selection: String? = nil
+    
     static var previews: some View {
         VStack {
-            PopupInputView(placeHolder: "Placeholder",value: "Value")
-            PopupInputView(placeHolder: "Placeholder",value: nil)
+            PopupInputView(placeHolder: "PlaceHolder", selectableItems: ["One", "Two", "Three"], selection: $selection)
         }
     }
 }
