567fe12b3ab7c66bdfe01037f1f859509a05885d
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-11-30 12:25:17 +0530

    schedule type test

--- StackOverFlow-AnswerTests/TeacherCourseTests.swift
+++ StackOverFlow-AnswerTests/TeacherCourseTests.swift
@@ -10,8 +10,6 @@ import SwiftUI
 @testable import StackOverFlow_Answer
 
 class TeacherCourseTests: XCTestCase {
-    var course: TeacherCourse!
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -20,11 +18,17 @@ class TeacherCourseTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testValiedCourse() {
-        let isValied = course.isValiedCourse
-        XCTAssertTrue(isValied, "Hesh: Course is not valied")
+    func testIsLiveOnceCourse() {
+        let onceCourse = TeacherCourse(title: "hgv", shortDescription: "jjh", description: "jhbjh", subCategory: .init(id: "swsw", code: "", name: "", parent: "", slug: "", dateAdded: "", lastModified: "", fontAwesomeClass: "", thumbnail: "", numberOfCourses: 2), level: .advanced, courseType: .live, classRoomType: .lecture, scheduleTitle: "jhbjhb", group: .once, courseDuration: .fourYear, startDay: .friday, startDate: Date(), startTime: Date(), endTime: Date(), languageMadeIn: .english, isTopCourse: false, requirements: ["kjn","hb"], outComes: ["lklk", "jnjn"], isFreeCourse: false, price: 200, discountFlag: false, discountedPrice: 200, courseOverViewProvider: .html5, overViewUrl: "url", thumbnail: Image("n"), metaKeywords: "bbnn", metaDescription: "nnnnn", courseID: " mjhbjh")
+        let isValied = onceCourse.isValiedCourse
+        XCTAssertTrue(isValied)
     }
-        
+    
+    func testIsLivePerWeekCourseFalse() {
+        let onceCourse = TeacherCourse(title: "hgv", shortDescription: "jjh", description: "jhbjh", subCategory: .init(id: "swsw", code: "", name: "", parent: "", slug: "", dateAdded: "", lastModified: "", fontAwesomeClass: "", thumbnail: "", numberOfCourses: 2), level: .advanced, courseType: .live, classRoomType: .lecture, scheduleTitle: "jhbjhb", group: .perWeek, courseDuration: nil, startDay: nil, startDate: Date(), startTime: Date(), endTime: Date(), languageMadeIn: .english, isTopCourse: false, requirements: ["kjn","hb"], outComes: ["lklk", "jnjn"], isFreeCourse: false, price: 200, discountFlag: false, discountedPrice: 200, courseOverViewProvider: .html5, overViewUrl: "url", thumbnail: Image("n"), metaKeywords: "bbnn", metaDescription: "nnnnn", courseID: " mjhbjh")
+        let isValied = onceCourse.isValiedCourse
+        XCTAssertFalse(isValied)
+    }
     func testPerformanceExample() throws {
         
     }
